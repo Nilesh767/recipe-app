cb6c0eccab60ecb20aa5284a60c6676646ae1a3d
Nilesh767 <nilesh.choudhary797@gmail.com>
2020-02-16 21:03:47 +0530

    Implemented Project Lombok.

--- src/main/java/com/n3o/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/n3o/recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.n3o.recipeapp.services;
 
 import com.n3o.recipeapp.domain.Recipe;
 import com.n3o.recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
     @Override
     public Set<Recipe> getRecipes() {
         Set<Recipe> recipeSet = new HashSet<>();
+        log.debug("you hu... i am called... recipe service boi!!!");
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
     }
