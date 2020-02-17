0827f93dd8a1ed81ac317e5bb89339485e2b628e
Nilesh767 <nilesh.choudhary797@gmail.com>
2020-02-17 20:17:39 +0530

    Integration Testing with DataJPA on unitOfMeasure

---
+++ src/test/java/com/n3o/recipeapp/repositories/UnitOfMeasureRepositoryITTest.java
@@ -0,0 +1,29 @@
+package com.n3o.recipeapp.repositories;
+
+import com.n3o.recipeapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+class UnitOfMeasureRepositoryITTest {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Test
+    void findByDescription() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon",unitOfMeasureOptional.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup",unitOfMeasureOptional.get().getDescription());
+    }
+}
