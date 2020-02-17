ef85a09a2b3aa0e9f93b8111194a6b72c6bb2c83
Nilesh767 <nilesh.choudhary797@gmail.com>
2020-02-17 19:22:26 +0530

    Did some testing with JUnit5 and used Mockito on the IndexController.

---
+++ src/test/java/com/n3o/recipeapp/controller/IndexControllerTest.java
@@ -0,0 +1,52 @@
+package com.n3o.recipeapp.controller;
+
+import com.n3o.recipeapp.domain.Recipe;
+import com.n3o.recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(3L);
+        recipes.add(recipe);
+        //when
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+        String s = indexController.getIndexPage(model);
+        //then
+        assertEquals("index",s);
+        verify(recipeService,times(1)).getRecipes();
+        verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2,setInController.size());
+    }
+}
