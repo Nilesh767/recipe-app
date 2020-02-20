837fd2ee672db1ffe3db6cf4c3b35b682b4e7ee1
Nilesh767 <nilesh.choudhary797@gmail.com>
2020-02-20 12:09:29 +0530

    basic refactoring.

--- src/main/java/com/n3o/recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/n3o/recipeapp/bootstrap/RecipeBootstrap.java
@@ -139,10 +139,8 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         guacRecipe.addIngredient(new Ingredient("Cilantro", new BigDecimal(2), tableSpoonUom));
         guacRecipe.addIngredient(new Ingredient("freshly grated black pepper", new BigDecimal(2), dashUom));
         guacRecipe.addIngredient(new Ingredient("ripe tomato, seeds and pulp removed, chopped", new BigDecimal(".5"), eachUom));
-
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
-
         guacRecipe.setUrl("http://www.simplyrecipes.com/recipes/perfect_guacamole/");
         guacRecipe.setServings(4);
         guacRecipe.setSource("Simply Recipes");
