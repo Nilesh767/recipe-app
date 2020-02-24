6975b48c050f026c49cbafc11cc15bac518c91de
Nilesh767 <nilesh.choudhary797@gmail.com>
2020-02-24 21:37:53 +0530

    image upload for the recipe.

---
+++ src/main/java/com/n3o/recipeapp/services/ImageService.java
@@ -0,0 +1,8 @@
+package com.n3o.recipeapp.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+   void saveImageFile(Long id, MultipartFile file);
+
+}
